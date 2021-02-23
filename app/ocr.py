ceaafbbbfca0c56e336c5470125e70311f6012ea
noahnisbet <noahnisbet3@gmail.com>
2021-02-22 19:38:36 -0500

    my first changes

--- app/ocr.py
+++ app/ocr.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ["OMP_NUM_THREADS"]= '1'
 os.environ["OMP_THREAD_LIMIT"] = '1'
 os.environ["MKL_NUM_THREADS"] = '1'
@@ -10,16 +11,35 @@ os.environ["OCR_THREADS"] = '1'
 
 from PIL import Image
 import pytesseract
-from pdf2image import convert_from_bytes
-from fastapi import APIRouter, File
+from pdf2image import convert_from_bytes, convert_from_path
+from fastapi import APIRouter, File, UploadFile
 import sqlalchemy
 from dotenv import load_dotenv, find_dotenv
 from sqlalchemy import create_engine
-from PyPDF2 import PdfFileReader
-
-from io import BytesIO
-
-import test
+import PyPDF4
+import io
+import slate3k as slate
+import poppler
+from app.BIA_Scraper import BIACase
+from typing import List, Tuple, Union, Callable, Dict, Iterator
+from collections import defaultdict
+from pprint import pprint
+from difflib import SequenceMatcher
+from datetime import datetime
+import bs4
+from bs4 import BeautifulSoup, element
+import geonamescache
+import requests
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import re
+import os
+import spacy
+from spacy.tokens.doc import Doc
+from spacy.tokens.span import Span
+from spacy.tokens.token import Token
+nlp = spacy.load('en_core_web_lg')
 
 router = APIRouter()
 # print('dir ', os.listdir('..'))
@@ -30,41 +50,88 @@ database_url = os.getenv('DATABASE_URL')
 engine = sqlalchemy.create_engine(database_url)
 
 @router.post('/insert')
-async def insertDoc(file: bytes = File(...)):
+async def create_upload_file(file: bytes = File(...)):
     '''
     This function inserts a PDF and the OCR converted text into a database
 
     '''
-    text = ocr_func(file)
-    # query = """INSERT INTO pdfs(pdf, plaintext) VALUES (%s, %s)""" 
-    # vals = (file, text)
-    # engine.execute(query, vals)
-    # Return text as response
-    return {'Text': text}
-
-
-def ocr_func(pdfBytes: bytes = File(...), txt_folder: str = './temp/'):
-    '''
-    Takes an uploaded .pdf file, converts it to plain text, and saves it as a
-    .txt file
-    range slice size can be changed to put more images in memory, but provides 
-    no speed boost, as the bottleneck is pytesseract.image_to_string
-    '''
-    fileReader = PdfFileReader(BytesIO(pdfBytes))
-    maxPages = fileReader.numPages
-    del fileReader
-    fulltext = []
-    for page in range(1, maxPages+1):
-        pil_image = convert_from_bytes(pdfBytes, dpi=300, first_page=page,
-                                            last_page=page, 
-                                            fmt= 'jpg',
-                                            thread_count=1, grayscale=True)
-        fulltext += [str(pytesseract.image_to_string(image)) for image in pil_image]
-        pil_image.clear()
-    return (''.join(fulltext).split('\n\n'))
     
-    # print('num pages', list(range(num_pages)))
-    # for i in range(num_pages):
-    #     filename = 'page_' + str(i) + '.jpg'
-    #     print('fileName', filename)
-        
+    text = []
+
+    ### Converts the bytes object recieved from fastapi
+    pages = convert_from_bytes(file,500)
+    
+    ### Uses pytesseract to convert each page of pdf to txt
+    for item in pages:
+        text.append(pytesseract.image_to_string(item))
+
+    ### Joins the list to an output string
+    string_to_return = " ".join(text)
+
+
+    return {'Text': string_to_return}
+
+@router.post('/get_fields')
+async def create_upload_file(file: bytes = File(...)):
+    
+
+    text = []
+
+    ### Converts the bytes object recieved from fastapi
+    pages = convert_from_bytes(file,500)
+    
+    ### Uses pytesseract to convert each page of pdf to txt
+    for item in pages:
+        text.append(pytesseract.image_to_string(item))
+
+    ### Joins the list to an output string
+    string = " ".join(text)
+
+
+    case = BIACase(string)
+    case_data = {}
+
+    remove_pw: Callable[[str], str]
+    remove_pw = lambda s: s[:s.find('?secret')]
+
+    case_data['filename'] = remove_pw(f) if 'password' in text else text[:-4]
+
+    app = case.get_application()
+    app = [ap for ap, b in app.items() if b]
+    case_data['application'] = '; '.join(app) if app else None
+
+    case_data['date'] = case.get_date()
+
+    case_data['country_of_origin'] = case.get_country_of_origin()
+
+    panel = case.get_panel()
+    case_data['panel_members'] = '; '.join(panel) if panel else None
+
+    case_data['outcome'] = case.get_outcome()
+
+    pgs = case.get_protected_grounds()
+    case_data['protected_grounds'] = '; '.join(pgs) if pgs else None
+
+    based_violence = case.get_based_violence()
+    violence = '; '.join([k for k, v in based_violence.items() if v]) \
+              if based_violence \
+              else None
+
+    keywords = '; '.join(['; '.join(v) for v in based_violence.values()]) \
+              if based_violence \
+              else None
+      
+    case_data['based_violence'] = violence
+    case_data['keywords'] = keywords
+
+    references = [
+    'Matter of AB, 27 I&N Dec. 316 (A.G. 2018)' 
+    if case.references_AB27_216() else None,
+    'Matter of L-E-A-, 27 I&N Dec. 581 (A.G. 2019)'
+    if case.references_LEA27_581() else None
+    ]
+
+    case_data['references'] = '; '.join([r for r in references if r])
+    case_data['sex_of_applicant'] = case.get_seeker_sex()
+
+    return case_data
